e9e60c3e54aa449b84cd4e4ac483d2c10f622267
guxuerui <im.guxuerui@gmail.com>
2023-04-08 22:40:10 +0800

    🐛 fix(firebase.client.ts): fix environment variable names to match Firebase configuration ✨ feat(firebase.client.ts): add support for Firebase app instance reuse to avoid memory leaks ✨ feat(firebase.client.ts): add Firebase auth instance export

--- src/lib/firebase/firebase.client.ts
+++ src/lib/firebase/firebase.client.ts
@@ -1,16 +1,27 @@
-import { initializeApp } from "firebase/app";
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth, setPersistence, inMemoryPersistence } from 'firebase/auth';
 
 // Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID,
   measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
-const app = initializeApp(firebaseConfig);
+let firebaseAPP;
+
+if (!getApps().length) {
+  firebaseAPP = initializeApp(firebaseConfig);
+} else {
+  firebaseAPP = getApp();
+  deleteApp(firebaseAPP)
+  firebaseAPP = initializeApp(firebaseConfig);
+}
+
+export const auth = getAuth(firebaseAPP);
