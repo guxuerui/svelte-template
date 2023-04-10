0a378d21f971465d3b7a3b5a113d515bc2a5d752
guxuerui <im.guxuerui@gmail.com>
2023-04-10 17:49:03 +0800

    ✨ feat(authStore.ts): add authStore and authHandlers for authentication functionality

--- src/lib/stores/authStore.ts
+++ src/lib/stores/authStore.ts
@@ -1,6 +1,13 @@
 import { writable } from "svelte/store";
 import { auth } from '$lib/firebase/firebase.client';
-import { createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, updateEmail, updatePassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+  updateEmail,
+  updatePassword,
+} from 'firebase/auth';
 
 export const authStore = writable({
   isLoading: true,
@@ -8,6 +15,9 @@ export const authStore = writable({
 })
 
 export const authHandlers = {
+  login: async (email: string, password: string) => {
+    await signInWithEmailAndPassword(auth, email, password)
+  },
   signup: async (email: string, password: string) => {
     await createUserWithEmailAndPassword(auth, email, password)
   },
@@ -15,9 +25,16 @@ export const authHandlers = {
     await signOut(auth)
   },
   resetPassword: async (email: string) => {
+    if (!email) { return }
     await sendPasswordResetEmail(auth, email)
   },
   updateEmail: async (email: string) => {
+    authStore.update(curr => {
+      return {
+        ...curr, currentUser: { ...curr.currentUser, email: email }
+      }
+    })
+
     await updateEmail(auth.currentUser, email)
   },
   updatePassword: async (password: string) => {
