94a670ffdf3b79eb620e5cb1acc84c0dde1884a3
guxuerui <im.guxuerui@gmail.com>
2023-05-10 14:03:46 +0800

    🎨 style(ToggleTheme.svelte): move toggle theme functionality to a separate component ✨ feat(Header.svelte): add ToggleTheme component to header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,74 +1,11 @@
 <script lang="ts">
-  import { onMount, tick } from "svelte";
   import { page } from "$app/stores";
   import logo from "$lib/images/svelte-logo.svg";
   import github from "$lib/images/github.svg";
   import { browser } from "$app/environment";
   import { authStore } from "$lib/stores/authStore";
   import { goto } from "$app/navigation";
-
-  $: currentTheme = "";
-
-  function setTheme() {
-    if (currentTheme === "light" || currentTheme === "auto") {
-      localStorage.setItem("color-schema", "dark");
-      document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("color-schema", "light");
-      document.documentElement.classList.remove("dark");
-    }
-  }
-
-  function toggleTheme(event?: MouseEvent) {
-    const isAppearanceTransition =
-      // @ts-expect-error experimental API
-      document?.startViewTransition &&
-      !window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-
-    if (!isAppearanceTransition) {
-      setTheme();
-      loadTheme();
-      return;
-    }
-
-    const x = event.clientX;
-    const y = event.clientY;
-    const endRadius = Math.hypot(
-      Math.max(x, innerWidth - x),
-      Math.max(y, innerHeight - y)
-    );
-    // @ts-expect-error: Transition API
-    const transition = document.startViewTransition(async () => {
-      setTheme();
-      loadTheme();
-      await tick();
-    });
-    transition.ready.then(() => {
-      console.log("currentTheme: ", currentTheme);
-      const clipPath = [
-        `circle(0px at ${x}px ${y}px)`,
-        `circle(${endRadius}px at ${x}px ${y}px)`,
-      ];
-      document.documentElement.animate(
-        {
-          clipPath:
-            currentTheme === "dark" ? [...clipPath].reverse() : clipPath,
-        },
-        {
-          duration: 500,
-          easing: "ease-out",
-          pseudoElement:
-            currentTheme === "dark"
-              ? "::view-transition-old(root)"
-              : "::view-transition-new(root)",
-        }
-      );
-    });
-  }
-
-  function loadTheme() {
-    currentTheme = localStorage.getItem("color-schema") || "auto";
-  }
+  import ToggleTheme from "$lib/components/ToggleTheme.svelte";
 
   function isLogin() {
     if (browser && !$authStore?.currentUser) {
@@ -77,10 +14,6 @@
     }
     goto("/about");
   }
-
-  onMount(() => {
-    loadTheme();
-  });
 </script>
 
 <header>
@@ -137,17 +70,7 @@
   </nav>
 
   <div class="flex">
-    <button
-      class="border-0 bg-transparent icon-btn px-0 !outline-none c-gray-800 hover:c-black dark:text-white"
-      on:click={toggleTheme}
-    >
-      {#if currentTheme === "light" || currentTheme === "auto"}
-        <div class="scale-180" i-carbon-sun />
-      {:else}
-        <div class="scale-180" i-carbon-moon />
-      {/if}
-    </button>
-
+    <ToggleTheme />
     <div class="corner">
       <a href="https://github.com/guxuerui/svelte-template">
         <img src={github} alt="GitHub" />
